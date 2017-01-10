9033e0dcdba47a14ef083dec7430ff77ab3774de
Salomon Beza <beza.salomon@gmail.com>
2017-01-10 14:51:18 +0200

    Second changes

--- story.rb
+++ story.rb
@@ -1,3 +1,6 @@
 puts "This is my first git story. I am going to make changes to my folder and track them. This will help me to learn more about git"
 
 puts "Now let's add anothef r line of code"
+
+puts "Now let's add the third line of 
+code"
